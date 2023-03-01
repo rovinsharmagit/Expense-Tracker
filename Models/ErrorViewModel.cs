b198bfcf9e1b7188f3e4e6d704679d9fc862fd67
rovinsharmagit <internetsharma45@gmail.com>
2023-03-01 14:41:58 +0530

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace my_expenses.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
