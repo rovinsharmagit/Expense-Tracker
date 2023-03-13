655a28561bcfbbec2cff09efa1b508026f701b31
rovinsharmagit <internetsharma45@gmail.com>
2023-03-13 18:34:08 +0530

    Working With Validation Of Forms

--- Models/Transaction.cs
+++ Models/Transaction.cs
@@ -4,15 +4,33 @@
     {
         [Key]
         public int TransactionId { get; set; }
-
+        [Range(1, int.MaxValue,ErrorMessage ="Please select a Category.")]
         public int CategoryId { get; set; }
         public Category? Category { get; set; }
-
+        [Range(1, int.MaxValue, ErrorMessage = "Amount should be greater than 0.")]
         public int Amount { get; set; }
 
         [Column(TypeName = "nvarchar(100)")]
         public string? Note { get; set; }
 
         public DateTime Date { get; set; } = DateTime.Now;
+
+        [NotMapped]
+        public string? CategoryTitleWithIcon
+        {
+            get
+            {
+                return Category==null? "" : Category.Icon + " " + Category.Title; 
+            }
+        }   
+
+        [NotMapped]
+        public string? FormattedAmount
+        {
+            get
+            {
+                return ((Category == null || Category.Type == "Expense")? "- " : "+ ") + Amount.ToString("0");
+            }
+        }
     }
 }
